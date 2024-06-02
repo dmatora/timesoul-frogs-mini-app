75a782ca4ea30f9d649304bdebccb0e6815005cb
Dmitry Matora <dmitry.matora@gmail.com>
2024-06-02 15:48:49 +0600

    Added Telegram Web App sdk

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
+import WebApp from '@twa-dev/sdk';
 
 import App from './app/app';
+
+WebApp.ready();
+WebApp.expand();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
