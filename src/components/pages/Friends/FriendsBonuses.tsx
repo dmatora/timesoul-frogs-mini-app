5df3dd8a6d19b74a56a3e41ee298f8b9ee7757c6
Dmitry Matora <dmitry.matora@gmail.com>
2024-06-12 02:42:18 +0600

    Disabled MoreBonuses

--- src/components/pages/Friends/FriendsBonuses.tsx
+++ src/components/pages/Friends/FriendsBonuses.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import FriendsHeader from './Bonuses/FriendsHeader';
 import FriendsBonusCard from './Bonuses/FriendsBonusCard';
-import MoreBonuses from './Bonuses/MoreBonus';
 
 const FriendsBonuses: React.FC = () => {
   return (
@@ -9,7 +8,8 @@ const FriendsBonuses: React.FC = () => {
       <FriendsHeader />
       <FriendsBonusCard />
       <FriendsBonusCard premium={true} />
-      <MoreBonuses>Больше бонусов</MoreBonuses>
+      {/*<MoreBonuses>Больше бонусов</MoreBonuses>*/}
+      {/* ToDo implement MoreBonuses */}
     </>
   );
 };
