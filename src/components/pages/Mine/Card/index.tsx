1fa89092483c2bb533c52592bba7ef3b2cc7c2f8
Dmitry Matora <dmitry.matora@gmail.com>
2024-06-18 21:28:18 +0600

    Implemented AJAX indication / buy blocking on card purchase

--- src/components/pages/Mine/Card/index.tsx
+++ src/components/pages/Mine/Card/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import Row from '../../../Row';
 import { Card as CardType, useFrogs } from '../../../../contexts/FrogsContext';
@@ -26,7 +26,7 @@ const CardLevel = styled.div`
   align-items: center;
 `;
 
-const BuyButton = styled.button`
+const BuyButton = styled.button<{ buying: boolean }>`
   width: 285px;
   height: 105px;
   border: none;
@@ -36,25 +36,64 @@ const BuyButton = styled.button`
   border-radius: 37px;
   box-shadow: 5px 6px 0 0 #262626;
 
-  visibility: ${({ disabled }) => (disabled ? 'hidden' : 'visible')};
+  visibility: ${({ disabled, buying }) => (disabled && !buying ? 'hidden' : 'visible')};
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: center;
+  ${({ buying }) =>
+    !buying &&
+    `
   &:active {
     position: relative;
     box-shadow: none;
     top: 6px;
     left: 5px;
   }
+  `};
+
+  ${({ buying }) =>
+    buying &&
+    `
+  &::after {
+    content: '.';
+    display: inline-block;
+    margin-left: 8px;
+    animation: dots 1.5s infinite;
+  }
+
+  @keyframes dots {
+    0%,
+    20% {
+      content: '.';
+    }
+    40% {
+      content: '..';
+    }
+    60% {
+      content: '...';
+    }
+    80%,
+    100% {
+      content: '';
+    }
+  }
+  `};
 `;
 
 const Card = ({ card, special = false }: { card: CardType; special?: boolean }) => {
+  const [buying, setBuying] = useState(false);
   const { buyCard, userCards } = useFrogs();
   const { t } = useTranslation();
   const userCard = userCards.find((userCard) => userCard.card_id === card.id);
   const cardLevel = userCard?.level_number || 0;
   const cardNextLevel = card.levels.find((level) => level.number === cardLevel + 1);
+
+  const handleOnClick = async () => {
+    setBuying(true);
+    await buyCard(card.id);
+    setBuying(false);
+  };
 
   return (
     <Container>
@@ -63,8 +102,8 @@ const Card = ({ card, special = false }: { card: CardType; special?: boolean }) 
         <CardLevel>
           {t('system.level')} {cardLevel}
         </CardLevel>
-        <BuyButton onClick={() => buyCard(card.id)} disabled={!cardNextLevel}>
-          {cardNextLevel && (
+        <BuyButton onClick={handleOnClick} buying={buying} disabled={buying || !cardNextLevel}>
+          {!buying && cardNextLevel && (
             <Row gap={'5px'}>
               <Coin />
               {cardNextLevel?.price}
